d733f64725a548195c80724f8e6d400d2b461af6
Nikita <nikita.sitlivy@mail.ru>
2026-03-19 15:57:28 +0300

    Деплой

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/table_AntD/',
   plugins: [react()],
   server: {
     port: 5174,
